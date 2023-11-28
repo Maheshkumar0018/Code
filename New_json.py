5ffa819ddae89917ff208f3d7be606477fc80cd1
Maheshkumar0018 <127117782+Maheshkumar0018@users.noreply.github.com>
2023-11-28 13:32:12 +0530

    Create New_json.py

---
+++ New_json.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+path_to_json = '/lala/'
+output_folder = '/modified_labels/'
+
+# Create the output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
+
+for file_name in [file for file in os.listdir(path_to_json) if file.endswith('.json')]:
+    input_path = os.path.join(path_to_json, file_name)
+    output_path = os.path.join(output_folder, file_name)
+
+    with open(input_path) as json_file:
+        data = json.load(json_file)
+
+        # Replace labels
+        for annotation in data['annotations']:
+            for obj in annotation['objects']:
+                if obj['label'] in ['apple', 'banana', 'grapes', 'orange', 'mango', 'dragon fruit']:
+                    obj['label'] = 'fruit'
+                elif obj['label'] in ['man', 'woman']:
+                    obj['label'] = 'non-fruit'
+
+        # Save the modified data to a new JSON file in the output folder
+        with open(output_path, 'w') as output_file:
+            json.dump(data, output_file, indent=2)
+
+print("Label replacement completed.")
